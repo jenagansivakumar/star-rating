924e4b934321de1e3bde72a66c6ca6936a44b8e0
Jena <jenagansivakumar@gmail.com>
2024-02-15 18:15:19 +0000

    logged the handle functions to see what i'm receiving and it's working

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -5,21 +5,28 @@ export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
-  function handleClick(getCurrentIndex) {}
+  function handleClick(getCurrentIndex) {
+    console.log(getCurrentIndex);
+  }
 
-  function handleMouseEnter(getCurrentIndex) {}
+  function handleMouseEnter(getCurrentIndex) {
+    console.log(getCurrentIndex);
+  }
 
-  function handleMouseLeave(getCurrentIndex) {}
+  function handleMouseLeave(getCurrentIndex) {
+    console.log(getCurrentIndex);
+  }
+
   return (
     <div className="star-rating">
-      {[...Array[noOfStars]].map((_, index) => {
-        index += 1;
+      {Array.from({ length: noOfStars }).map((_, index) => {
+        const currentIndex = index + 1;
         return (
           <FaStar
-            key={index}
-            onClick={handleClick(index)}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave(index)}
+            key={currentIndex}
+            onClick={() => handleClick(currentIndex)}
+            onMouseEnter={() => handleMouseEnter(currentIndex)}
+            onMouseLeave={() => handleMouseLeave(currentIndex)}
             size={40}
           />
         );
