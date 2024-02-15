09e89428c1c3422055b105d3e867e8b9ee2c65fa
Jena <jenagansivakumar@gmail.com>
2024-02-15 18:05:48 +0000

    adding state for the rating

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating({ noOfStars = 5 }) {
+
+    const [rating, setRating] = useState(0)
   return (
     <div className="star-rating">
       {[...Array[noOfStars]].map((_, index) => {
-        return <FaStar />;
+        return <FaStar
+        key={index}
+        onClick={}
+        onMouseEnter={}
+        onMouseLeave={}
+        size={40}
+        />;
       })}
     </div>
   );
