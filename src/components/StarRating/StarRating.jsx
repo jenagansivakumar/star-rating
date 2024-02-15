e5ec9a8a7e2639884204b994e849e80bdc024108
Jena <jenagansivakumar@gmail.com>
2024-02-15 18:03:46 +0000

    creating logic for onclick, on mouse enter and on mouse leave

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -1,5 +1,11 @@
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating({ noOfStars = 5 }) {
-  return <div className="star-rating">{[...Array[noOfStars]].map}</div>;
+  return (
+    <div className="star-rating">
+      {[...Array[noOfStars]].map((_, index) => {
+        return <FaStar />;
+      })}
+    </div>
+  );
 }
