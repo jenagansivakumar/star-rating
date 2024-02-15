95846e2bd8ed7e21252b376837a39cf87fe080d6
Jena <jenagansivakumar@gmail.com>
2024-02-15 18:00:57 +0000

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import StarRating from "./components/StarRating/StarRating";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <StarRating />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/StarRating/StarRating.jsx
@@ -0,0 +1,5 @@
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating({ noOfStars = 5 }) {
+  return <div className="star-rating">{[...Array[noOfStars]].map}</div>;
+}
