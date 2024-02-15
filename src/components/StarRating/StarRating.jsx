c4e192c59a67070d7d745e04a15bb9b6cbea03c1
Jena <jenagansivakumar@gmail.com>
2024-02-15 18:12:26 +0000

    added handle functions to the events

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -2,19 +2,27 @@ import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
-    const [rating, setRating] = useState(0)
-    const [hover, setHover] = useState(0)
+  function handleClick(getCurrentIndex) {}
+
+  function handleMouseEnter(getCurrentIndex) {}
+
+  function handleMouseLeave(getCurrentIndex) {}
   return (
     <div className="star-rating">
       {[...Array[noOfStars]].map((_, index) => {
-        return <FaStar
-        key={index}
-        onClick={}
-        onMouseEnter={}
-        onMouseLeave={}
-        size={40}
-        />;
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            onClick={handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
       })}
     </div>
   );
