876fbca245a8b7010de67e84a7c6a18e788f0259
Jena <jenagansivakumar@gmail.com>
2024-02-15 18:06:43 +0000

    added useState for hover

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -4,6 +4,7 @@ import { FaStar } from "react-icons/fa";
 export default function StarRating({ noOfStars = 5 }) {
 
     const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
   return (
     <div className="star-rating">
       {[...Array[noOfStars]].map((_, index) => {
